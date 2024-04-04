fa53158316cb0bd10df5e70d9c688dfb73ec924e
Ankit Dua <ankit.dua@part1.in>
2024-04-04 19:13:32 +0530

    First commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontSize: {
+        2.5: "0.625rem",
+        8: "2rem",
+      },
     },
   },
   plugins: [],
